96b06b4f0bc4422341f34b1be6694c1c4a9f0283
Vitor Santos <vitor93gs@gmail.com>
2024-02-23 14:57:08 -0300

    fix id

--- python-script.py
+++ python-script.py
@@ -13,11 +13,11 @@ sql_query_template_final = """
 
 # Open a file to write the SQL queries
 with open('insert_queries.sql', 'w' ,encoding='utf-8') as f:
-    f.write("""INSERT INTO Productivity (cidade, estado, jan, fev, mar, abr, mai, jun, jul, ago, set, out, nov, dez, anual)
+    f.write("""INSERT INTO Productivity (id, cidade, estado, janeiro, fevereiro, marco, abril, maio, junho, julho, agosto, setembro, outubro, novembro, dezembro, anual)
 VALUES """)
     # Iterate through DataFrame rows and write queries to the file
    
-    for index, row in df.iterrows():
+    for i, row in df.iterrows():
         # Format the query with row data
         data = {
             'xpd' : row.to_dict()['CIDADE,ESTADO,JAN,FEV,MAR,ABR,MAI,JUN,JUL,AGO,SET,OUT,NOV,DEZ,ANUAL']
@@ -27,12 +27,20 @@ VALUES """)
         new_string_with_double_quotes = query.replace("'","''")
         new_string = re.sub(r'\(([^,]+),([^,]+)', r"('\1','\2'", new_string_with_double_quotes)
             
-        # Write the query to the file
-        if index == df.size-1:
+        if i == df.size-1:
             final_string_with_double_quotes = sql_query_template_final.format(**data).replace("'","''")
             final_string = re.sub(r'\(([^,]+),([^,]+)', r"('\1','\2'", final_string_with_double_quotes)
-            f.write(final_string)
+
+            index = final_string.find("(")
+            if index != -1:
+                f.write(final_string[:index+1] + str(i) + ',' + final_string[index+1:])
+            else:
+                f.write(final_string)
         else:
-            f.write(new_string)
+            index = new_string.find("(")
+            if index != -1:
+                f.write(new_string[:index+1] + str(i) + ',' + new_string[index+1:])
+            else:
+                f.write(new_string)
 
 print("SQL queries generated and saved to insert_queries.sql")
